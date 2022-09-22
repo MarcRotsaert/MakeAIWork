4070c4568ae7836b59a1aade29bfc1d87c029ae7
MarcRotsaert <mglrot@caiway.nl>
2022-09-22 22:38:41 +0200

    project 1  220922 deel b

--- simpylc/mro_simpylc.sh
+++ simpylc/mro_simpylc.sh
@@ -3,5 +3,5 @@
 export LIBGL_ALLOW_SOFTWARE=1
 python -m simpylc -a 
 #python simulations/car/world.py 
-python /C/temp//__simulations//car//control_client//hardcoded_client.py 
-#python simulations/car/control_client/hardcoded_client.py 
+#python /C/temp//__simulations//car//control_client//hardcoded_client.py 
+python simulations/car/control_client/hardcoded_client.py 
